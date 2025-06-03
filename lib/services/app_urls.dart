efba6954c8eb43b37a452ccb38c64525f5d1fff7
James Edward <jamesnavigatot04@gmail.com>
2025-06-03 22:48:02 +0300

    updates

--- lib/services/app_urls.dart
+++ lib/services/app_urls.dart
@@ -1,7 +1,7 @@
 class AppUrl {
   static const String liveUrl = "https://alltanzaniaecard.onrender.com";
 
-  static const String localhost = "http://192.168.203.45:8080";
+  static const String localhost = "http://192.168.1.151:8080";
 
   // authentication endpoints here
   static const String baseEndpoint = localhost;
@@ -16,6 +16,7 @@ class AppUrl {
   static String getAllCardsById = "$baseEndpoint/api/v1/cards/user-cards";
   static var updateCard = "$baseEndpoint/api/v1/cards/update";
   static var deleteCard = "$baseEndpoint/api/v1/cards/delete";
+  static var saveCard = "$baseEndpoint/api/v1/saved-cards/save-card";
 
   static String createCard = '$baseEndpoint/api/v1/cards/create';
 }
